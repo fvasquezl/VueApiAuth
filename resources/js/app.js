e07ffadcac8b293a72731627d8ad0b25d6b54bf4
faustino_vasquez <faustino.vasquez@mitechnologiesinc.com>
2020-02-26 14:53:21 -0800

    Vuetify installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+const app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+});
